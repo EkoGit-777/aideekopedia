e4fcbe7e6bfd3933c7d87ade4eb69eb256a96c17
Eko Sutrisno Adiguna <73046155+EkoGit-777@users.noreply.github.com>
2025-10-04 02:33:43 +0700

    remove logto for now

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,21 +2,20 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@unocss/nuxt', '@logto/nuxt', '@pinia/nuxt'],
+  modules: [
+    '@unocss/nuxt',
+    // '@logto/nuxt',
+    '@pinia/nuxt'
+  ],
   css: ['~/assets/tailwind.css'],
   runtimeConfig: {
-    logto: {
-      endpoint: process.env.LOGTO_ENDPOINT,
-      appId: process.env.LOGTO_APP_ID,
-      appSecret: process.env.LOGTO_APP_SECRET,
-      cookieEncryptionKey: process.env.LOGTO_COOKIE_ENCRYPTION_KEY,
-    },
+    // logto: {
+    //   endpoint: process.env.LOGTO_ENDPOINT,
+    //   appId: process.env.LOGTO_APP_ID,
+    //   appSecret: process.env.LOGTO_APP_SECRET,
+    //   cookieEncryptionKey: process.env.LOGTO_COOKIE_ENCRYPTION_KEY,
+    // },
     googleGeminiApiKey: process.env.GOOGLE_GENERATIVE_AI_API_KEY,
-  },
-  nitro: {
-    externals: {
-      inline: ['@logto/node']
-    }
   },
   logto: {
     pathnames: {
